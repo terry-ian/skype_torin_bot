da10ef978922a732f185cf2913eb89737b3f39cc
terry-ian <39936938+terry-ian@users.noreply.github.com>
2024-09-18 15:30:50 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -76,7 +76,7 @@ def handle_message():
         sender = data['conversation']['id']
         text_gpt = chat_session.send_message(data['text'])
         text = text_gpt.text.replace('**','').replace('*','-')
-        text = text.replace('\n\n', '\n').replace('\n\n\n', '\n').rstrip('\n')
+        text = text.replace('\n\n', '\n').replace('\n\n\n', '\n').replace('\n\n\n\n', '\n').rstrip('\n')
         bot.send_message(bot_id, bot_name, recipient, service, sender, text)
         
     except Exception as e:
