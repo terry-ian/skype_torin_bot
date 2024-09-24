c281f9f6010c86f753ab8b45d65b86e879304262
terry-ian <39936938+terry-ian@users.noreply.github.com>
2024-09-24 15:36:06 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,13 +12,43 @@ app_id = os.getenv('APP_ID')
 app_secret = os.getenv('APP_SECRET')
 genai.configure(api_key="AIzaSyD8OG39WRzCydCU2l6qOmqLJkldMbFmI9o")
 
+def upload_to_gemini(path, mime_type=None):
+    """Uploads the given file to Gemini.
+
+    See https://ai.google.dev/gemini-api/docs/prompting_with_media
+    """
+    file = genai.upload_file(path, mime_type=mime_type)
+    print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+    return file
+
+def wait_for_files_active(files):
+    """Waits for the given files to be active.
+
+    Some files uploaded to the Gemini API need to be processed before they can be
+    used as prompt inputs. The status can be seen by querying the file's "state"
+    field.
+
+    This implementation uses a simple blocking polling loop. Production code
+    should probably employ a more sophisticated approach.
+    """
+    print("Waiting for file processing...")
+    for name in (file.name for file in files):
+        file = genai.get_file(name)
+        while file.state.name == "PROCESSING":
+            print(".", end="", flush=True)
+            time.sleep(10)
+            file = genai.get_file(name)
+        if file.state.name != "ACTIVE":
+            raise Exception(f"File {file.name} failed to process")
+    print("...all files ready")
+    print()
 
 # Create the model
 generation_config = {
-  "temperature": 1,
+  "temperature": 0.3,
   "top_p": 0.95,
   "top_k": 64,
-  "max_output_tokens": 1500,
+  "max_output_tokens": 12000,
   "response_mime_type": "text/plain",
 }
 
@@ -28,6 +58,8 @@ model = genai.GenerativeModel(
   # safety_settings = Adjust safety settings
   # See https://ai.google.dev/gemini-api/docs/safety-settings
 )
+files = [./data/combined_output.txt", mime_type="text/plain"),
+        ]
 
 chat_session = model.start_chat(
   history=[
@@ -40,13 +72,13 @@ chat_session = model.start_chat(
     {
       "role": "user",
       "parts": [
-        "超出FAQ範圍請說: 請詢問關於atZone相關問題",
+        files[0],
       ],
     },
     {
-      "role": "model",
+      "role": "user",
       "parts": [
-        "很抱歉，我無法回答這個問題。它超出了我的 FAQ 範圍。\n\n請詢問關於 atZone 相關問題。\n",
+        "回答麻煩使用繁體中文。",
       ],
     },
   ]
